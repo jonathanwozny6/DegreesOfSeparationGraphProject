03b57291b9d724bb279fca8e6f9ebb4cb45c677f
jonathanwozny6 <71037154+jonathanwozny6@users.noreply.github.com>
2021-12-06 22:38:40 -0500

    Update cFrame.cpp

--- cFrame.cpp
+++ cFrame.cpp
@@ -5,14 +5,18 @@
 #include <set>
 #include <iostream>
 
+// Event table for clicking stuff
 wxBEGIN_EVENT_TABLE(cFrame, wxFrame)
     EVT_BUTTON(10001, cFrame::OnButtonClicked)
+    EVT_LISTBOX_DCLICK(10002, cFrame::URLButtonClicked)
 wxEND_EVENT_TABLE()
 
 cFrame::cFrame() : wxFrame(nullptr, wxID_ANY, "Degrees of Separation", wxPoint(30, 30), wxSize(500, 400))
 {
+    // Find Path Button
     FPBtn = new wxButton(this, 10001, "Find Path", wxPoint(50,110), wxSize(100,80));
 
+    // Refer to cFrame.h to see meaning of each object
     FrUser = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10,30), wxSize(200,30));
     FrUseLbl = new wxStaticText(this, wxID_ANY, "Enter From User", wxPoint(10, 10), wxSize(200, 30));
     TrgUser = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10,90), wxSize(200,30));
@@ -22,34 +26,73 @@ cFrame::cFrame() : wxFrame(nullptr, wxID_ANY, "Degrees of Separation", wxPoint(3
     PthLbl = new wxStaticText(this, wxID_ANY, "Path: ", wxPoint(10, 170), wxSize(200, 30));
     TotConLbl = new wxStaticText(this, wxID_ANY, "Max Deg. of Separation: ", wxPoint(310, 40), wxSize(200, 30));
 
-    PthList = new wxListBox(this, wxID_ANY, wxPoint(10,190), wxSize(460,150));
+    PthList = new wxListBox(this, 10002, wxPoint(10,190), wxSize(460,150));
 
-    oldFrUser = -1;
+    oldFrUser = -1; // set to default -1 for first run
+
+    CreateStatusBar(); // Status Bar --> Good to use for error messages on bottom of frame
 
     // create graph
     //Graph("twitter-2010.txt");
-    Graph("musae_git_edges.csv");
+    Graph("musae_git_edges.csv"); // Github graph
     //Graph("nodes.txt");
     //Graph("newNodes.txt");
 }
 
+// Destructor --> not used
 cFrame::~cFrame()
 {
 
 }
 
+// Double Click in Path List button
+void cFrame::URLButtonClicked(wxCommandEvent &evt)
+{
+    // convert wxstring object that was double-clicked to integer
+    int userNum = wxAtoi(PthList->GetString(PthList->GetSelection()).ToStdString());
+
+    // find name in map to use in url
+    std::string name = userMap[userNum];
+
+    // open the url for the selected person
+    openURL("https://github.com/" + name);
+    evt.Skip(); // tell the computer that this event has been handled
+}
+
+// https://stackoverflow.com/questions/4177744/c-os-x-open-default-browser
+void cFrame::openURL(const std::string &url_str) {
+    CFURLRef url = CFURLCreateWithBytes (
+            NULL,                        // allocator
+            (UInt8*)url_str.c_str(),     // URLBytes
+            url_str.length(),            // length
+            kCFStringEncodingASCII,      // encoding
+            NULL                         // baseURL
+    );
+    LSOpenCFURLRef(url,0);
+    CFRelease(url);
+}
+
+// Find Path button
 void cFrame::OnButtonClicked(wxCommandEvent &evt)
 {
     // get connections to list in wxListBox
     int in, out, result;
+
+    // convert user to integer
     in = wxAtoi(FrUser->GetValue());
+
+    // THIS SHOULD BE CHANGED LATER TO ONLY ALLOW CHARACTERS/STRINGS TO BE INPUT
+    // 0 is the default value when nothing is input or string is input
     if (in == 0 && FrUser->GetValue() != "0") {
-        std::cout << "Must Input Integer Value" << std::endl;
+        SetStatusText("Must Input Integer Value"); // error message
         return;
     }
     else {
-        out = wxAtoi(TrgUser->GetValue());
-        std::vector<int> path = DijkstrasConnect(in, out);
+        SetStatusText("");
+        out = wxAtoi(TrgUser->GetValue()); // convert target user to integer
+
+        // find the path
+        std::vector<unsigned int> path = DijkstrasConnect(in, out);
 
         // clear the list box
         PthList->Clear();
@@ -62,6 +105,7 @@ void cFrame::OnButtonClicked(wxCommandEvent &evt)
             // print from user
             PthList->AppendString(std::to_string(in));
 
+            // if not max value (aka -1)
             if (path[0] != -1) {
                 // print users in between
                 for (int i = path.size() - 1; i > -1; i--)
@@ -73,14 +117,16 @@ void cFrame::OnButtonClicked(wxCommandEvent &evt)
 
         if (in != oldFrUser)
         {
+            // if the From User has been changed, re-calculate largest degree of separation
             result = BFSConnect(in, out);
+
             if (result == -1)
-                TotCon->SetLabel("'From User' not Found");
+                TotCon->SetLabel("'From User' not Found"); // error message
             else
-               TotCon->SetLabel(std::to_string(result));
+               TotCon->SetLabel(std::to_string(result)); // display the result
         }
 
-        oldFrUser = in;
+        oldFrUser = in; // old user is now what was input previously
     }
     evt.Skip(); // tell computer that this event has been handled
 }
@@ -102,7 +148,7 @@ void cFrame::ReadInput(const std::string& fileName)
     bool opened = true;
     if (!input){
         opened = false;
-        std::cout << "Unable to open input file" << std::endl;
+        std::cout << "Unable to open input file 1" << std::endl;
     }
 
     if (opened)
@@ -115,18 +161,41 @@ void cFrame::ReadInput(const std::string& fileName)
         // while the end of the file is not yet reached
         while (!input.eof()) {
             input >> user;
-            //input >> comma;
+            input >> comma;
             input >> following;
             if (user+1 > adjList.size())
                 adjList.resize(user+1);
             adjList[user].insert(following);
             if (following + 1 > adjList.size())
                 adjList.resize(following+1);
-            adjList[following].insert(user);
+            adjList[following].insert(user); // make it undirected graph
+        }
+        input.close();
+    }
+
+    // create map between nodes and usernames
+    input.open("musae_git_target.csv");
+    opened = true;
+    if (!input){
+        opened = false;
+        std::cout << "Unable to open input file 2" << std::endl;
+    }
+    if (opened)
+    {
+        unsigned int numUser;
+        std::string userName;
+        char comma;
+        int junk; // extra binary digit at end
+
+        // while the end of the file is not yet reached
+            // there are no spaces between commas and strings so must use getline
+        while (input >> numUser >> comma && getline(input, userName, ',') && input >> junk) {
+            userMap[numUser] = userName;
         }
 
         input.close();
     }
+
 }
 
 void cFrame::Follow(const unsigned int user, const unsigned int following)
@@ -166,7 +235,6 @@ unsigned int cFrame::BFSConnect(const unsigned int user, const unsigned int targ
     // variable to store number of connections (distance away from source vertex)
     unsigned int numConn = 0;
 
-
     // while the whole graph has been searched and that target has not been found
     while (!q.empty())
     {
@@ -199,15 +267,14 @@ unsigned int cFrame::BFSConnect(const unsigned int user, const unsigned int targ
         numConn++;
     }
 
+    // farthest reach in breadth first search
     return numConn-1;
-    return UINT_MAX;
 }
 
-std::vector<int> cFrame::DijkstrasConnect(const unsigned int user, const unsigned int target)
+std::vector<unsigned int> cFrame::DijkstrasConnect(const unsigned int user, const unsigned int target)
 {
-    // your code
     // path vector to return
-    std::vector<int> path;
+    std::vector<unsigned int> path;
 
     if (!CheckConnected(user, target))
         return path;
@@ -279,6 +346,7 @@ std::vector<int> cFrame::DijkstrasConnect(const unsigned int user, const unsigne
             }
         }
 
+        // the target value has been found
         if (v == target)
             found = true;
     }
@@ -319,7 +387,6 @@ bool cFrame::CheckConnected(const unsigned int user, const unsigned int target)
         return false;
 
 // use BFS
-
     // create queue and push the start
     std::queue<unsigned int> q;
     q.push(user);
@@ -353,5 +420,31 @@ bool cFrame::CheckConnected(const unsigned int user, const unsigned int target)
     if (numUsers == visited.size())
         return true;
     else return false;
+}
 
+// returns the number of mutual friends a user has
+std::set<unsigned int> cFrame::GetMutualFriends(const unsigned int user1, const unsigned int user2)
+{
+    // create set
+    std::set<unsigned int> mutual;
+
+    //
+    for (int x : adjList[user1])
+    {
+        if (adjList[user2].find(x) != adjList[user2].end())
+        mutual.insert(x);
+        }
+
+    return mutual;
+}
+
+// returns the set of friends that a user has
+std::set<unsigned int> cFrame::GetFriends(const unsigned int user)
+{
+    return adjList[user];
+}
+
+unsigned int cFrame::GetNumFriends(const unsigned int user)
+{
+    return adjList[user].size();
 }
